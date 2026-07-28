d90dd8afbebf8168c29ded1dece9934d42e9a37b
ahmedoujdi <manuelsanchezperez886@gmail.com>
2026-07-28 23:24:32 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./theme";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
-);
