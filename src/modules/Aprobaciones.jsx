1839272debff6752fc4d6c75932791b8043eb3b4
ahmedoujdi <manuelsanchezperez886@gmail.com>
2026-08-01 17:31:39 +0200

    Fix import path for shared module in Aprobaciones

--- src/modules/Aprobaciones.jsx
+++ src/modules/Aprobaciones.jsx
@@ -36,7 +36,7 @@ import {
   DateRangeFilter, 
   StatusBadge,
   formatTimestamp 
-} from "./shared.jsx";
+} from "../shared.jsx";
 
 export default function Aprobaciones({ user }) {
   const [approvals, setApprovals] = useState([]);
